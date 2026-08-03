d20f6a5c564d6f7ed7dcf7590906e484c0641852
Solomon Umeh <soloking2forsure@gmail.com>
2026-08-03 19:14:37 +0100

    testing API calls and cors for api

--- client-dating-app/src/app/app.ts
+++ client-dating-app/src/app/app.ts
@@ -1,5 +1,12 @@
-import { Component, signal } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Component, computed, inject, signal } from '@angular/core';
+import { toSignal } from '@angular/core/rxjs-interop';
 
+interface IMember {
+  id: string;
+  name: string;
+  email: string;
+}
 
 @Component({
   selector: 'app-root',
@@ -8,5 +15,8 @@ import { Component, signal } from '@angular/core';
   styleUrl: './app.css',
 })
 export class App {
-  protected readonly title = signal('client-dating-app');
+  private readonly http = inject(HttpClient);
+  protected readonly title = signal('Client App');
+  private readonly membersOb = toSignal(this.http.get('https://localhost:5001/api/members'));
+  protected members = computed(() => this.membersOb() as IMember[]);
 }
